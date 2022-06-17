6e426dd536087fca26279fc7beec0c115e257820
riazakhanda017ksn <riazkahanda@gmail.com>
2022-06-17 06:57:50 -0700

    initial channge

--- mern-ecommerce/src/components/CartItem/CartItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CartItem = ({ data }) => {
-  return <></>;
-};
-
-export default CartItem;
--- mern-ecommerce/src/components/Seemore/Seemore.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Seemore = () => {
-  return <div></div>;
-};
-
-export default Seemore;
