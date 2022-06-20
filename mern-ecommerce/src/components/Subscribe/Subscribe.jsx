963deffa0f9192296832df6c5cf8b83158c896f8
riazakhanda017ksn <riazkahanda@gmail.com>
2022-06-20 14:16:25 -0700

    aster

--- mern-ecommerce/src/components/Subscribe/Subscribe.jsx
+++ mern-ecommerce/src/components/Subscribe/Subscribe.jsx
@@ -1,30 +1,30 @@
-import React from "react";
-import videoOne from "../../video/jjj.mp4";
-import "./Subscribe.css";
-import SendIcon from "@mui/icons-material/Send";
-const Subscribe = () => {
-  return (
-    <>
-      <div className="video-section">
-        <video src={videoOne} muted loop autoPlay></video>
-        <div className="video-of-overly">
-          <h1>Subscribe our site and get regular update</h1> <br />
-          <div>
-            <form action="">
-              <div className="subscribe__content">
-                <input type="email" placeholder="Your email...." />
+// import React from "react";
+// import videoOne from "../../video/jjj.mp4";
+// import "./Subscribe.css";
+// import SendIcon from "@mui/icons-material/Send";
+// const Subscribe = () => {
+//   return (
+//     <>
+//       <div className="video-section">
+//         <video src={videoOne} muted loop autoPlay></video>
+//         <div className="video-of-overly">
+//           <h1>Subscribe our site and get regular update</h1> <br />
+//           <div>
+//             <form action="">
+//               <div className="subscribe__content">
+//                 <input type="email" placeholder="Your email...." />
 
-                <button>
-                  <SendIcon />
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <></>
-      </div>
-    </>
-  );
-};
+//                 <button>
+//                   <SendIcon />
+//                 </button>
+//               </div>
+//             </form>
+//           </div>
+//         </div>
+//         <></>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Subscribe;
+// export default Subscribe;
