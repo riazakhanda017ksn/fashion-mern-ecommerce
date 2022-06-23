e990417a78d61ffcb617039687a2ecb1856fb25c
riazakhanda017ksn <riazkahanda@gmail.com>
2022-06-23 11:45:50 -0700

    readme file added

---
+++ mern-ecommerce/src/components/Messenger/Messenger.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+// const Messenger = () => {
+//     return (
+//         <>
+//     <MessengerCustomerChat
+//     pageId="104966515599669"
+//     appId="<APP_ID>"
+//   /> 
+//         </>
+//     );
+// };
+
+// export default Messenger;
